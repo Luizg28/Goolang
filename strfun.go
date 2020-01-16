29db20a865664509d672f4bae429942d501c2475
Luiz Lopes <54402979+Luizg28@users.noreply.github.com>
2020-01-16 10:15:45 -0400

    Add files via upload

---
+++ strfun.go
@@ -0,0 +1,9 @@
+package main; //The package i am using
+
+import"fmt"; //The import you need to print in go.
+
+func main(){ //The main function
+	fmt.Print("O começo de tudo!"); //This one print and does not go to the next line
+	fmt.Println("Em Frente \"Testando\"!"); //This Print and go to de next line and the \ make de second and third " do not count as programing language.
+	fmt.Println("Luiz Gabriel","Maciel Lopes");
+}
