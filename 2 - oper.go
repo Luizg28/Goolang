ec7d8c0807bb292e2226f23b62c5c805d20f07ca
Luiz Lopes <54402979+Luizg28@users.noreply.github.com>
2020-01-16 10:51:13 -0400

    Add files via upload

---
+++ 2 - oper.go
@@ -0,0 +1,11 @@
+package main;
+
+import "fmt";
+
+func main(){
+	fmt.Println(10 + 10); // Testing the +
+	fmt.Println(20.6 - 10.4 + 8.3); // Testing the -
+	fmt.Println(70 * -5); // Testing the * and seeing if the - works making negative number
+	fmt.Println(300 / 6); // Testing the /
+	fmt.Println(10 % 3); // Testing the %
+}
---
+++ 3 - var.go
@@ -0,0 +1,19 @@
+package main;
+
+import "fmt";
+
+func main(){
+	var inteiro int = 20; // Creating and giving value to a variable
+	var texto string = 	"Um texto pra testes"; // A string
+	var seTipo = "Uma string diferente"; 
+
+	decimal := 15.5; // A diferent way to creat a variable
+	boolean := true; // A boolean
+
+	//Printing the values
+	fmt.Println(inteiro);
+	fmt.Println(texto);
+	fmt.Println(seTipo);
+	fmt.Println(decimal);
+	fmt.Println(boolean);
+}
